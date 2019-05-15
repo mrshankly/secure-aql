e24af1f6e5c51ce5fd49b28a6f800ed9535a47cf
PDCosta <pd.costa@campus.fct.unl.pt>
2019-05-15 17:27:13 +0100

    Fixing variables

--- src/join.erl
+++ src/join.erl
@@ -5,20 +5,20 @@
 
 -export([scan/3]).
 
-scan(Table, ?PARSER_WILDCARD, TxId, Table2) ->
-  TName = table:name(Table),
-  TName2 = table:name(Table2),
-   lists:map(fun({_Key, BObj}) -> BObj end, TName2);
+%scan(Table, ?PARSER_WILDCARD, TxId, Table2) ->
+%  TName = table:name(Table),
+%  TName2 = table:name(Table2),
+%   lists:map(fun({_Key, BObj}) -> BObj end, TName2);
 
-scan(Table, Conditions, TxId) ->
-  evaluate(Table, Conditions, TxId, []).
+%scan(Table, Conditions, TxId) ->
+%evaluate(Table, Conditions, TxId, []).
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
 
 
-evaluate(Table, [], TxId, Acc) ->
-  Acc. 
+%evaluate(Table, [], TxId, Acc) ->
+ % Acc. 
 
 
