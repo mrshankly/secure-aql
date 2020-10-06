501acb9aa0ac7997d70184a300e4f955da20b327
João Marques <jb.marques@campus.fct.unl.pt>
2020-10-06 19:40:08 +0100

    Allow clients to request table metadata

--- src/aql_protocol.erl
+++ src/aql_protocol.erl
@@ -7,7 +7,9 @@
 
 -include_lib("kernel/include/logger.hrl").
 
+-include("aql.hrl").
 -include("aql_pb.hrl").
+-include("types.hrl").
 
 start_link(Ref, Transport, Opts) ->
     Pid = spawn_link(?MODULE, init, [Ref, Transport, Opts]),
@@ -30,21 +32,63 @@ loop(Socket, Transport) ->
             ok = Transport:close(Socket)
     end.
 
-handle_message(Socket, Transport, _Request = #'Request'{type = 'QUERY', query = Query}) ->
-    Response =
-        case aql:query(binary_to_list(Query)) of
-            {ok, []} ->
-                #'QueryResponse'{response = <<"ok">>};
-            {ok, Result} ->
-                #'QueryResponse'{response = list_to_binary(Result)};
-            {ok, [], _Transaction} ->
-                #'QueryResponse'{response = <<"ok">>};
-            {ok, Result, _Transaction} ->
-                #'QueryResponse'{response = list_to_binary(Result)};
-            {error, Reason} ->
-                #'QueryResponse'{error = term_to_binary(Reason)}
-        end,
+handle_message(Socket, Transport, #'Request'{type = 'QUERY', query = Query}) ->
+    Response = run_query(binary_to_list(Query)),
+    Transport:send(Socket, aql_pb:encode_msg(Response));
+handle_message(Socket, Transport, #'Request'{type = 'METADATA', tables = Tables}) ->
+    Response = get_metadata(split_tables(Tables)),
+    Transport:send(Socket, aql_pb:encode_msg(Response));
+handle_message(Socket, Transport, #'Request'{
+    type = 'QUERY_AND_METADATA',
+    query = Query,
+    tables = Tables
+}) ->
+    Response = get_metadata(run_query(binary_to_list(Query)), split_tables(Tables)),
     Transport:send(Socket, aql_pb:encode_msg(Response));
 handle_message(_Socket, _Transport, Request) ->
     ?LOG_INFO("unknown request: ~p", [Request]),
     ok.
+
+split_tables(TableNames) ->
+    lists:map(fun binary_to_atom/1, string:split(TableNames, ",", all)).
+
+run_query(Query) ->
+    run_query(#'Response'{}, Query).
+
+run_query(Response, Query) ->
+    case aql:query(binary_to_list(Query)) of
+        {ok, []} ->
+            Response#'Response'{query = <<"ok">>};
+        {ok, Result} ->
+            Response#'Response'{query = list_to_binary(Result)};
+        {ok, [], _Transaction} ->
+            Response#'Response'{query = <<"ok">>};
+        {ok, Result, _Transaction} ->
+            Response#'Response'{query = list_to_binary(Result)};
+        {error, Reason} ->
+            Response#'Response'{query_error = term_to_binary(Reason)};
+        {error, Reason, _Transaction} ->
+            Response#'Response'{query_error = term_to_binary(Reason)}
+    end.
+
+get_metadata(TableNames) ->
+    get_metadata(#'Response'{}, TableNames).
+
+get_metadata(Response, TableNames) ->
+    {ok, Transaction} = antidote_handler:start_transaction(),
+    Tables = table:read_tables(Transaction),
+    antidote_handler:commit_transaction(Transaction),
+
+    GetColumnsMetadata = fun(TableName) ->
+        case proplists:get_value(?MAP_KEY(TableName, ?META_CRDT_TYPE), Tables) of
+            undefined ->
+                undefined;
+            Table when ?is_table(Table) ->
+                {TableName, table:columns(Table)}
+        end
+    end,
+
+    AllMetadata = lists:map(GetColumnsMetadata, TableNames),
+    Metadata = lists:filter(fun(Table) -> Table =/= undefined end, AllMetadata),
+
+    Response#'Response'{metadata = erlang:term_to_binary(Metadata)}.
