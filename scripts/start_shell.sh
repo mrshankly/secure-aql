fdb81626151d12ab6520d0cbd842b9e8b45a2e3c
João Marques <jb.marques@campus.fct.unl.pt>
2021-03-13 17:02:12 +0000

    Fix start_shell.sh script

--- scripts/start_shell.sh
+++ scripts/start_shell.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
-if [ -z "$NODE_NAME" ]; then
+set - e
+
+if [ -z "${NODE_NAME}" ]; then
 	export NODE_NAME='aql@127.0.0.1'
 fi
 
-if [ -z "$AQL_REL" ]; then
+if [ -z "${AQL_REL}" ]; then
 	# by default, this script will be executed from the Makefile
 	export AQL_REL=_build/default/rel/aql
 fi
 
-# echo "Using AQL node name: $NODE_NAME"
+echo "Using AQL node name: ${NODE_NAME} (use Ctrl+D to exit)"
 
-$AQL_REL/bin/env daemon && sleep 5 && $AQL_REL/bin/env eval "aql:start_shell()."
+${AQL_REL}/bin/env daemon && sleep 5
+
+echo 'aql:start_shell().' | ${AQL_REL}/bin/env daemon_attach >/dev/null 2>&1
+${AQL_REL}/bin/env daemon_attach 2>/dev/null
+
+${AQL_REL}/bin/env stop
